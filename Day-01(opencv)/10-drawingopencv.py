c880ff00a7da32cae549f24db583aef7f7e376d6
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2025-04-14 13:57:55 +0600

    complete drawing using opencv

--- Day-01(opencv)/10-drawingopencv.py
+++ Day-01(opencv)/10-drawingopencv.py
@@ -17,7 +17,7 @@ cv2.rectangle(img, (200, 250), (350, 350), (0, 0, 225), -1)
 ### circle
 
 ## measuring shape is (671, 1000, 3) meaning that y cordinate or height and x cordinate or width
-### so below (500) is vertical or x axis and (550) is horizontal or y axis  
+### so below (275) is vertical or x axis and (300) is horizontal or y axis  
 
 cv2.circle(img, (275, 300), 95, (255, 0, 0), 3) ##BGR
 cv2.circle(img, (800, 200), 95, (255, 0, 0), 3) 
@@ -25,9 +25,7 @@ cv2.circle(img, (800, 200), 95, (255, 0, 0), 3)
 
 ### text
 
-
-
-
+cv2.putText(img, 'Hey You bro !!!!', (300, 550), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 255), 7)
 
 
 cv2.imshow('img', img)
