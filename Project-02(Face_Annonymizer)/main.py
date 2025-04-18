e59c4d9595b9de4e03569a9d4c67cb0a66f7521d
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2025-04-18 15:51:45 +0600

    wrapped up in a function-face annonymizer

--- Project-02(Face_Annonymizer)/main.py
+++ Project-02(Face_Annonymizer)/main.py
@@ -1,24 +1,16 @@
+import os
+
 import cv2
 import mediapipe as mp
 
-# read image
-img_path = './data/she.jpg'
-img = cv2.imread(img_path)
 
+### This function is for video annonymization ###
 
-H, W, _ = img.shape
-
-
-# detect face
-
-mp_face_detection = mp.solutions.face_detection
-
-with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5) as face_detection:
-    # pass
+def process_img(img, face_detection):
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     out = face_detection.process(img_rgb)
 
-    print(out.detections)
+    # print(out.detections)
 
     if out.detections is not None:
         for detection in out.detections: 
@@ -32,13 +24,36 @@ with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence
             w = int(w * W)
             h = int(h * H)
 
-            img = cv2.rectangle(img, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 10)
+            # img = cv2.rectangle(img, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 10)
     
-    cv2.imshow('img', img)
-    cv2.waitKey(0)
+            # blur faces
+            img[y1:y1 + h, x1:x1 + w, :] = cv2.blur(img[y1:y1 + h, x1:x1 + w, :], (40, 40) )
 
-# blur faces
+    return img
+
+output_dir = './output'
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# read image
+img_path = './data/she.jpg'
+img = cv2.imread(img_path)
 
 
+H, W, _ = img.shape
+
+# detect face
+
+mp_face_detection = mp.solutions.face_detection
+
+with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5) as face_detection:
+    # pass
+    img = process_img(img, face_detection)
+   
+
+
+    # cv2.imshow('img', img)
+    # cv2.waitKey(0)
 
 # save image
+cv2.imwrite(os.path.join(output_dir, 'she_output.png'), img)
